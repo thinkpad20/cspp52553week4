6d21c40e8487a513b395bc7593c6e876ce5e3331
Jeff Cohen <jeff@starterleague.com>
2013-04-24 21:37:53 -0500

    Fix routes file

--- lecture_code/config/routes.rb
+++ lecture_code/config/routes.rb
@@ -3,15 +3,13 @@ MoviesApp::Application.routes.draw do
   get '/movies', controller: 'movies', action: 'index', as: 'movies'
 
   get '/movies/new', controller: 'movies', action: 'new', as: 'new_movie'
+  post '/movies', controller: 'movies', action: 'create'
 
   get '/movies/:id', controller: 'movies', action: 'show', as: 'movie'
 
   get '/movies/:id/edit', controller: 'movies', action: 'edit', as: 'edit_movie'
   put '/movies/:id', controller: 'movies', action: 'update'
 
-  post '/movies', controller: 'movies', action: 'create'
-
-  get 'delete_movie/:id'
   delete '/movies/:id', controller: 'movies', action: 'destroy'
 
 end
